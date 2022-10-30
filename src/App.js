f41b2486cea7af1ae9811ab7961754658bc3b5ca
Yashpriyadeep katta <yashkatta2000@gmail.com>
2022-10-30 19:26:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './ICS_logo_cube.png';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <img src={logo} class="top_logo" alt="ICS" width = "500" height = "500"/>
+      <h1 className='text'>Internet Computer Services Demo React App</h1>
+    </div>
+  );
+}
+
+export default App;
